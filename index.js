0cf5024fe565013d6d8b37b2f085d194165fdbef
Paul Andrei Cielo <andreiciexo@gmail.com>
2025-09-06 08:58:55 +0800

    Initial web server code

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const server = createServer((req, res) => {
   res.end('Hello World');
 });
 server.listen(port, hostname, () => {
-  console.log(Server running at http://${hostname}:${port}/);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
